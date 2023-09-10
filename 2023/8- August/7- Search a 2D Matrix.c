aa448a12a456927081cbdbf18abeb68484cadc11
zyadhany <zeyad.hany2003@gmail.com>
2023-09-11 02:10:09 +0300

    added another solve for day 7 of month 8

--- 2023/8- August/7- Search a 2D Matrix.c
+++ 2023/8- August/7- Search a 2D Matrix.c
@@ -9,3 +9,23 @@ public:
         return (0);
     }
 };
+
+
+/*solve using upper and lower pound*/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix[0][0] > target) return (0);
+
+        int l, r;
+        vector<int> Y = { target, INT32_MAX };
+
+        l = upper_bound(matrix.begin(), matrix.end(), Y) - matrix.begin() - 1;
+        r = lower_bound(matrix[l].begin(), matrix[l].end(), target) - matrix[l].begin();
+
+        if (r >= matrix[0].size()) return(0);
+
+        return (matrix[l][r] == target);
+    }
+};
